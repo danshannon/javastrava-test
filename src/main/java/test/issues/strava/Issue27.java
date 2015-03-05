f82281f7f13cbb70f38424855f8ee6db54b1c9d5
danshannon <danshannon@optusnet.com.au>
2015-03-05 14:35:09 +1100

    javastrava-api #27 issue test See https://github.com/danshannon/javastravav3api/issues/27

---
+++ src/main/java/test/issues/strava/Issue27.java
@@ -0,0 +1,29 @@
+package test.issues.strava;
+
+import static org.junit.Assert.assertNull;
+import javastrava.api.v3.model.StravaSegmentEffort;
+import javastrava.api.v3.service.exception.NotFoundException;
+import javastrava.api.v3.service.impl.retrofit.Retrofit;
+import javastrava.api.v3.service.impl.retrofit.SegmentEffortServicesRetrofit;
+
+import org.junit.Test;
+
+import test.utils.TestUtils;
+
+/**
+ * <p>
+ * These tests should PASS if issue <a href="https://github.com/danshannon/javastravav3api/issues/32">javastrava-api #32</a> still exists
+ * </p>
+ * 
+ * @author Dan Shannon
+ * @see <a href="https://github.com/danshannon/javastravav3api/issues/32">https://github.com/danshannon/javastravav3api/issues/32</a>
+ *
+ */
+public class Issue27 {
+	@Test
+	public void testIssue() throws NotFoundException {
+		SegmentEffortServicesRetrofit retrofit = Retrofit.retrofit(SegmentEffortServicesRetrofit.class, TestUtils.getValidToken());
+		StravaSegmentEffort effort = retrofit.getSegmentEffort(5591276015L);
+		assertNull(effort.getAthlete().getResourceState());
+	}
+}
