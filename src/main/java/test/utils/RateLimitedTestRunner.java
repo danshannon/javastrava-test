2514b1192a2d3a2d66e0f4ef0d9d26a7da98a810
Dan Shannon <d.shannon.au@gmail.com>
2015-03-23 03:17:38 +1100

    Back off testing on 503 Service Unavailable error

--- src/main/java/test/utils/RateLimitedTestRunner.java
+++ src/main/java/test/utils/RateLimitedTestRunner.java
@@ -5,12 +5,15 @@ package test.utils;
 
 import javastrava.api.v3.service.Strava;
 import javastrava.api.v3.service.exception.StravaAPIRateLimitException;
+import javastrava.api.v3.service.exception.StravaServiceUnavailableException;
 
 /**
  * @author danshannon
  *
  */
 public class RateLimitedTestRunner {
+	private final static Strava strava = TestUtils.strava();
+
 	public static void run(final TestCallback t) throws Exception {
 		boolean loop = true;
 		while (loop) {
@@ -19,13 +22,37 @@ public class RateLimitedTestRunner {
 				loop = false;
 			} catch (final StravaAPIRateLimitException e) {
 				waitForRateLimit();
+			} catch (final StravaServiceUnavailableException e) {
+				waitForServiceRestoration();
 			}
 		}
 	}
 
+	/**
+	 *
+	 */
+	private static void waitForServiceRestoration() {
+		boolean loop = true;
+		while (loop) {
+			try {
+				System.out.println("WARN - Strava temporarily unavailable (503 error) - pausing execution for 60 seconds");
+				Thread.sleep(60000l);
+			} catch (final InterruptedException e) {
+				// ignore
+			}
+			try {
+				strava.getAuthenticatedAthlete();
+				// If the call works, then we didn't get a service unavailable exception so we're good to go
+				loop = false;
+			} catch (final StravaServiceUnavailableException e) {
+				loop = true;
+			}
+		}
+
+	}
+
 	private static void waitForRateLimit() {
 		boolean loop = true;
-		final Strava strava = TestUtils.strava();
 		while (loop) {
 			try {
 				System.out.println("WARN - Rate limit exceeded - pausing test execution for 15 seconds");
