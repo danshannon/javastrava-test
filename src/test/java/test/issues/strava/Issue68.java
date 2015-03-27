9dfbbb6dae6b55ec314c8592bc2aee8cbaffc4e3
Dan Shannon <dan.shannon@marlo.com.au>
2015-03-27 16:00:51 +1100

    Issue test for javastrava-api #68 See https://github.com/danshannon/javastravav3api/issues/68

--- src/test/java/test/issues/strava/Issue68.java
+++ src/test/java/test/issues/strava/Issue68.java
@@ -5,10 +5,10 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import javastrava.api.v3.model.StravaPhoto;
 import javastrava.api.v3.rest.API;
-import javastrava.api.v3.service.exception.NotFoundException;
 
 import org.junit.Test;
 
+import test.utils.RateLimitedTestRunner;
 import test.utils.TestUtils;
 
 /**
@@ -17,18 +17,19 @@ import test.utils.TestUtils;
  */
 public class Issue68 {
 	@Test
-	public void testIssue() throws NotFoundException {
-		API api = new API(TestUtils.getValidTokenWithViewPrivate());
-		
-		// This one should work - it's a photo uploaded to Instagram
-		StravaPhoto[] instagram = api.listActivityPhotos(245713183);
-		
-		// This one doesn't work - it's a photo uploaded via the Strava iPhone app
-		StravaPhoto[] iPhoneApp = api.listActivityPhotos(244461140);
-		
-		assertNotNull(instagram);
-		assertFalse(instagram.length == 0);
-		
-		assertNull(iPhoneApp);
+	public void testIssue() throws Exception {
+		RateLimitedTestRunner.run(() -> {
+
+			API api = new API(TestUtils.getValidTokenWithViewPrivate());
+
+			// This one should work - it's a photo uploaded to Instagram
+			StravaPhoto[] instagram = api.listActivityPhotos(245713183);
+
+			// This one doesn't work - it's a photo uploaded via the Strava iPhone app
+			StravaPhoto[] iPhoneApp = api.listActivityPhotos(244461140);
+			assertNotNull(instagram);
+			assertFalse(instagram.length == 0);
+			assertNull(iPhoneApp);
+		});
 	}
 }
