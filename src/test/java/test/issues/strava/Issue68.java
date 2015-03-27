3ed5c3e0def8d03d099d5242b2e812c6af364d37
Dan Shannon <dan.shannon@marlo.com.au>
2015-03-27 15:36:34 +1100

    Issue test for issue #68 See https://github.com/danshannon/javastravav3api/issues/68

---
+++ src/test/java/test/issues/strava/Issue68.java
@@ -0,0 +1,34 @@
+package test.issues.strava;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import javastrava.api.v3.model.StravaPhoto;
+import javastrava.api.v3.rest.API;
+import javastrava.api.v3.service.exception.NotFoundException;
+
+import org.junit.Test;
+
+import test.utils.TestUtils;
+
+/**
+ * @author Dan Shannon
+ *
+ */
+public class Issue68 {
+	@Test
+	public void testIssue() throws NotFoundException {
+		API api = new API(TestUtils.getValidTokenWithViewPrivate());
+		
+		// This one should work - it's a photo uploaded to Instagram
+		StravaPhoto[] instagram = api.listActivityPhotos(245713183);
+		
+		// This one doesn't work - it's a photo uploaded via the Strava iPhone app
+		StravaPhoto[] iPhoneApp = api.listActivityPhotos(244461140);
+		
+		assertNotNull(instagram);
+		assertFalse(instagram.length == 0);
+		
+		assertNull(iPhoneApp);
+	}
+}
