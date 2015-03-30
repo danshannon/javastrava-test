23a4418dd157946bbd88b7e5e617fba5f127c7d8
Dan Shannon <d.shannon.au@gmail.com>
2015-03-31 00:54:34 +1100

    Issue test for javastrava-api #73 See https://github.com/danshannon/javastravav3api/issues/73

---
+++ src/test/java/test/issues/strava/Issue73.java
@@ -0,0 +1,39 @@
+/**
+ *
+ */
+package test.issues.strava;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import javastrava.api.v3.model.StravaSegmentLeaderboard;
+import javastrava.api.v3.model.reference.StravaResourceState;
+import javastrava.api.v3.rest.API;
+
+import org.junit.Test;
+
+import test.utils.RateLimitedTestRunner;
+import test.utils.TestUtils;
+
+/**
+ * <p>
+ * Issue test for javastrava-api #73
+ * </p>
+ *
+ * <p>
+ * Tests will PASS if the issue is still current
+ * </p>
+ *
+ * @author Dan Shannon
+ * @see <a href="https://github.com/danshannon/javastravav3api/issues/73">https://github.com/danshannon/javastravav3api/issues/73</a>
+ */
+public class Issue73 {
+	@Test
+	public void issueTest() throws Exception {
+		RateLimitedTestRunner.run(() -> {
+			final API api = new API(TestUtils.getValidToken());
+			final StravaSegmentLeaderboard leaderboard = api.getSegmentLeaderboard(1190741, null, null, null, null, null, null, null, null, null);
+			assertNotNull(leaderboard);
+			assertEquals(StravaResourceState.PRIVATE, leaderboard.getResourceState());
+		});
+	}
+}
