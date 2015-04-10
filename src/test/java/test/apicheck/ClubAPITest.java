a164c93e2a22a414053a21d23ef3a0acd48ef4a5
Dan Shannon <dan.shannon@marlo.com.au>
2015-04-10 16:00:02 +1000

    Add API check for list club announcements (issue javastravav3api#100)

--- src/test/java/test/apicheck/ClubAPITest.java
+++ src/test/java/test/apicheck/ClubAPITest.java
@@ -2,15 +2,15 @@ package test.apicheck;
 
 import java.io.IOException;
 
+import org.junit.Test;
+
 import javastrava.api.v3.model.StravaActivity;
 import javastrava.api.v3.model.StravaAthlete;
 import javastrava.api.v3.model.StravaClub;
+import javastrava.api.v3.model.StravaClubAnnouncement;
 import javastrava.api.v3.rest.API;
 import javastrava.api.v3.service.exception.NotFoundException;
 import javastrava.json.exception.JsonSerialisationException;
-
-import org.junit.Test;
-
 import retrofit.client.Response;
 import test.apicheck.api.ClubAPI;
 import test.apicheck.api.ResponseValidator;
@@ -21,32 +21,38 @@ import test.utils.TestUtils;
  *
  */
 public class ClubAPITest {
-	@Test
-	public void testAPI_getClub() throws NotFoundException, JsonSerialisationException, IOException {
-		Response response = api().getClub(TestUtils.CLUB_VALID_ID);
-		ResponseValidator.validate(response, StravaClub.class, "getClub");
-	}
-	
-	@Test
-	public void testAPI_listAuthenticatedAthleteClubs() throws JsonSerialisationException, IOException {
-		Response response = api().listAuthenticatedAthleteClubs();
-		ResponseValidator.validate(response, StravaClub.class, "listAuthenticatedAthleteClubs");
-	}
-	
-	@Test
-	public void testAPI_listClubMembers() throws JsonSerialisationException, IOException, NotFoundException {
-		Response response = api().listClubMembers(TestUtils.CLUB_VALID_ID, null, null);
-		ResponseValidator.validate(response, StravaAthlete.class, "listClubMembers");
-	}
-	
-	@Test
-	public void testAPI_listRecentClubactivities() throws JsonSerialisationException, IOException, NotFoundException {
-		Response response = api().listRecentClubActivities(TestUtils.CLUB_VALID_ID, null, null);
-		ResponseValidator.validate(response, StravaActivity.class, "listRecentClubActivities");
-	}
-	
 	private ClubAPI api() {
 		return API.instance(ClubAPI.class, TestUtils.getValidToken());
+	}
+
+	@Test
+	public void testAPI_getClub() throws NotFoundException, JsonSerialisationException, IOException {
+		final Response response = api().getClub(TestUtils.CLUB_VALID_ID);
+		ResponseValidator.validate(response, StravaClub.class, "getClub");
+	}
+
+	@Test
+	public void testAPI_listAuthenticatedAthleteClubs() throws JsonSerialisationException, IOException {
+		final Response response = api().listAuthenticatedAthleteClubs();
+		ResponseValidator.validate(response, StravaClub.class, "listAuthenticatedAthleteClubs");
+	}
+
+	@Test
+	public void testAPI_listClubAnnouncements() throws JsonSerialisationException, IOException {
+		final Response response = api().listClubAnnouncements(TestUtils.CLUB_VALID_ID);
+		ResponseValidator.validate(response, StravaClubAnnouncement.class, "listClubAnnouncements");
+	}
+
+	@Test
+	public void testAPI_listClubMembers() throws JsonSerialisationException, IOException, NotFoundException {
+		final Response response = api().listClubMembers(TestUtils.CLUB_VALID_ID, null, null);
+		ResponseValidator.validate(response, StravaAthlete.class, "listClubMembers");
+	}
+
+	@Test
+	public void testAPI_listRecentClubactivities() throws JsonSerialisationException, IOException, NotFoundException {
+		final Response response = api().listRecentClubActivities(TestUtils.CLUB_VALID_ID, null, null);
+		ResponseValidator.validate(response, StravaActivity.class, "listRecentClubActivities");
 	}
 
 }
--- src/test/java/test/apicheck/api/ClubAPI.java
+++ src/test/java/test/apicheck/api/ClubAPI.java
@@ -1,8 +1,9 @@
 /**
- * 
+ *
  */
 package test.apicheck.api;
 
+import javastrava.api.v3.service.ClubService;
 import javastrava.api.v3.service.exception.NotFoundException;
 import retrofit.client.Response;
 import retrofit.http.GET;
@@ -13,52 +14,76 @@ import retrofit.http.Query;
  * <p>
  * API definition of the endpoints for club services
  * </p>
- * 
+ *
  * @author Dan Shannon
  *
  */
 public interface ClubAPI {
 	/**
 	 * @see javastrava.api.v3.service.ClubService#getClub(java.lang.Integer)
-	 * 
-	 * @param id Club identifier
+	 *
+	 * @param id
+	 *            Club identifier
 	 * @return Club details
-	 * @throws NotFoundException If the club with the given id doesn't exist
+	 * @throws NotFoundException
+	 *             If the club with the given id doesn't exist
 	 */
 	@GET("/clubs/{id}")
 	public Response getClub(@Path("id") final Integer id) throws NotFoundException;
 
 	/**
 	 * @see javastrava.api.v3.service.ClubService#listAuthenticatedAthleteClubs()
-	 * 
+	 *
 	 * @return Array of clubs that the authenticated athlete belongs to
 	 */
 	@GET("/athlete/clubs")
 	public Response listAuthenticatedAthleteClubs();
 
 	/**
-	 * @see javastrava.api.v3.service.ClubService#listClubMembers(Integer, javastrava.util.Paging)
-	 * 
-	 * @param id CLub identifier
-	 * @param page Page number to be returned (default is 1)
-	 * @param perPage Page size to be returned (default is 50)
-	 * @return Array of athletes who are members of the identified club
-	 * @throws NotFoundException If the club with the given id doesn't exist
+	 * @see ClubService#listClubAnnouncements(Integer)
+	 *
+	 * @param clubId
+	 *            Id of the club to list announcements for
+	 * @return Array of announcements
 	 */
-	@GET("/clubs/{id}/members")
-	public Response listClubMembers(@Path("id") final Integer id, @Query("page") final Integer page, @Query("per_page") final Integer perPage) throws NotFoundException;
+	@GET("/clubs/{id}/announcements")
+	public Response listClubAnnouncements(@Path("id") Integer clubId);
 
 	/**
-	 * @see javastrava.api.v3.service.ClubService#listRecentClubActivities(Integer, javastrava.util.Paging)
-	 * 
-	 * @param id Club identifier
-	 * @param page Page number to be returned (default is 1)
-	 * @param perPage Page size to be returned (default is 50)
-	 * @return Array of activities recently done by club members (maximum 200 will be returned)
-	 * @throws NotFoundException If the club with the given id doesn't exist
+	 * @see javastrava.api.v3.service.ClubService#listClubMembers(Integer,
+	 *      javastrava.util.Paging)
+	 *
+	 * @param id
+	 *            CLub identifier
+	 * @param page
+	 *            Page number to be returned (default is 1)
+	 * @param perPage
+	 *            Page size to be returned (default is 50)
+	 * @return Array of athletes who are members of the identified club
+	 * @throws NotFoundException
+	 *             If the club with the given id doesn't exist
+	 */
+	@GET("/clubs/{id}/members")
+	public Response listClubMembers(@Path("id") final Integer id, @Query("page") final Integer page,
+			@Query("per_page") final Integer perPage) throws NotFoundException;
+
+	/**
+	 * @see javastrava.api.v3.service.ClubService#listRecentClubActivities(Integer,
+	 *      javastrava.util.Paging)
+	 *
+	 * @param id
+	 *            Club identifier
+	 * @param page
+	 *            Page number to be returned (default is 1)
+	 * @param perPage
+	 *            Page size to be returned (default is 50)
+	 * @return Array of activities recently done by club members (maximum 200
+	 *         will be returned)
+	 * @throws NotFoundException
+	 *             If the club with the given id doesn't exist
 	 */
 	@GET("/clubs/{id}/activities")
-	public Response listRecentClubActivities(@Path("id") final Integer id, @Query("page") final Integer page, @Query("per_page") final Integer perPage)
-			throws NotFoundException;
+	public Response listRecentClubActivities(@Path("id") final Integer id, @Query("page") final Integer page,
+			@Query("per_page") final Integer perPage) throws NotFoundException;
 
 }
