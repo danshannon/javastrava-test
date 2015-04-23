fa4d690b526acefbf905d6b2b58b07c5b5d2ef9d
Dan Shannon <dan.shannon@marlo.com.au>
2015-04-23 10:57:36 +1000

    Paging test updates

--- src/main/java/test/api/service/impl/util/PagingListMethodTest.java
+++ src/main/java/test/api/service/impl/util/PagingListMethodTest.java
@@ -7,50 +7,31 @@ import static org.junit.Assert.fail;
 
 import java.util.List;
 
+import org.junit.Test;
+
 import javastrava.api.v3.model.reference.StravaResourceState;
 import javastrava.api.v3.service.exception.UnauthorizedException;
 import javastrava.util.Paging;
-
-import org.junit.Test;
-
 import test.api.service.StravaTest;
 import test.utils.RateLimitedTestRunner;
 
 /**
  * <p>
- * Utility test class used for testing all the service method implementations that return lists
+ * Utility test class used for testing all the service method implementations
+ * that return lists
  * </p>
  *
  * @author Dan Shannon
  *
  * @param <T>
- *            Class of objects contained in the lists returned by the method being tested
+ *            Class of objects contained in the lists returned by the method
+ *            being tested
  * @param <U>
- *            Class of the object's identifier (mostly they're Integer, but some are Long or even String)
+ *            Class of the object's identifier (mostly they're Integer, but some
+ *            are Long or even String)
  */
 public abstract class PagingListMethodTest<T, U> extends StravaTest {
-	/**
-	 * <p>
-	 * Test paging (page size only)
-	 * </p>
-	 *
-	 * @throws Exception
-	 *
-	 * @throws UnauthorizedException
-	 *             Thrown when security token is invalid
-	 */
-	@Test
-	public void testPageSize() throws Exception {
-		RateLimitedTestRunner.run(() -> {
-			// Get a list with only one entry
-				final List<T> list = callback().getList(new Paging(1, 1));
-				assertNotNull(list);
-				assertEquals(1, list.size());
-
-				// Validate all the entries in the list
-				validateList(list);
-			});
-	}
+	protected abstract ListCallback<T> callback();
 
 	/**
 	 * <p>
@@ -58,8 +39,9 @@ public abstract class PagingListMethodTest<T, U> extends StravaTest {
 	 * </p>
 	 *
 	 * <p>
-	 * To test this we get 2 entities from the service, then ask for the first page only and check that it's the same as the first
-	 * entity, then ask for the second page and check that it's the same as the second entity
+	 * To test this we get 2 entities from the service, then ask for the first
+	 * page only and check that it's the same as the first entity, then ask for
+	 * the second page and check that it's the same as the second entity
 	 * </p>
 	 *
 	 * @throws Exception
@@ -83,26 +65,52 @@ public abstract class PagingListMethodTest<T, U> extends StravaTest {
 			assertEquals(1, secondPage.size());
 			validateList(secondPage);
 
-			// The first entry in bothPages should be the same as the first entry in firstPage
-				assertEquals(bothPages.get(0), firstPage.get(0));
+			// The first entry in bothPages should be the same as the first
+			// entry in firstPage
+			assertEquals(bothPages.get(0), firstPage.get(0));
 
-				// The second entry in bothPages should be the same as the first entry in secondPage
-				assertEquals(bothPages.get(1), secondPage.get(0));
+			// The second entry in bothPages should be the same as the first
+			// entry in secondPage
+			assertEquals(bothPages.get(1), secondPage.get(0));
 
-			});
+		} );
+	}
+
+	/**
+	 * <p>
+	 * Test paging (page size only)
+	 * </p>
+	 *
+	 * @throws Exception
+	 *
+	 * @throws UnauthorizedException
+	 *             Thrown when security token is invalid
+	 */
+	@Test
+	public void testPageSize() throws Exception {
+		RateLimitedTestRunner.run(() -> {
+			// Get a list with only one entry
+			final List<T> list = callback().getList(new Paging(1, 1));
+			assertNotNull(list);
+			assertEquals(1, list.size());
+
+			// Validate all the entries in the list
+			validateList(list);
+		} );
 	}
 
 	@Test
 	public void testPageSizeTooLargeForStrava() throws Exception {
 		RateLimitedTestRunner.run(() -> {
-			// Get a list with too many entries for Strava to cope with in a single paging instruction
-				final List<T> list = callback().getList(new Paging(2, 201));
-				assertNotNull(list);
-				assertTrue(list.size() <= 201);
+			// Get a list with too many entries for Strava to cope with in a
+			// single paging instruction
+			final List<T> list = callback().getList(new Paging(2, 201));
+			assertNotNull(list);
+			assertTrue(list.size() <= 201);
 
-				// Validate all the entries in the list
-				validateList(list);
-			});
+			// Validate all the entries in the list
+			validateList(list);
+		} );
 	}
 
 	@Test
@@ -117,11 +125,11 @@ public abstract class PagingListMethodTest<T, U> extends StravaTest {
 			assertEquals(1, list.size());
 
 			// The first entry in the list should be the second entry in bigList
-				assertEquals(bigList.get(1), list.get(0));
+			assertEquals(bigList.get(1), list.get(0));
 
-				// Validate all the entries in the list
-				validateList(bigList);
-			});
+			// Validate all the entries in the list
+			validateList(bigList);
+		} );
 	}
 
 	@Test
@@ -136,16 +144,40 @@ public abstract class PagingListMethodTest<T, U> extends StravaTest {
 			assertEquals(1, list.size());
 
 			// The first entry in the list should be the first entry in bigList
-				assertEquals(bigList.get(0), list.get(0));
+			assertEquals(bigList.get(0), list.get(0));
 
-				// Validate all the entries in the list
-				validateList(bigList);
-			});
+			// Validate all the entries in the list
+			validateList(bigList);
+		} );
 	}
 
 	/**
 	 * <p>
-	 * Test paging for paging parameters that can't return values (i.e. are out of range - too low)
+	 * Test paging for paging parameters that can't return values (i.e. are out
+	 * of range - too high)
+	 * </p>
+	 *
+	 * @throws Exception
+	 *
+	 * @throws UnauthorizedException
+	 *             Thrown when security token is invalid
+	 */
+	@Test
+	public void testPagingOutOfRangeHigh() throws Exception {
+		RateLimitedTestRunner.run(() -> {
+			// Get the 200,000,000th entry in the list - this is pretty unlikely
+			// to return anything!
+			final List<T> list = callback().getList(new Paging(1000000, 200));
+
+			assertNotNull(list);
+			assertEquals(0, list.size());
+		} );
+	}
+
+	/**
+	 * <p>
+	 * Test paging for paging parameters that can't return values (i.e. are out
+	 * of range - too low)
 	 * </p>
 	 *
 	 * @throws Exception
@@ -163,33 +195,12 @@ public abstract class PagingListMethodTest<T, U> extends StravaTest {
 				return;
 			}
 			fail("Allowed paging instruction for page -1 of size -1!");
-		});
+		} );
 	}
-
-	/**
-	 * <p>
-	 * Test paging for paging parameters that can't return values (i.e. are out of range - too high)
-	 * </p>
-	 *
-	 * @throws Exception
-	 *
-	 * @throws UnauthorizedException
-	 *             Thrown when security token is invalid
-	 */
-	@Test
-	public void testPagingOutOfRangeHigh() throws Exception {
-		RateLimitedTestRunner.run(() -> {
-			// Get the 200,000,000th entry in the list - this is pretty unlikely to return anything!
-				final List<T> list = callback().getList(new Paging(1000000, 200));
-
-				assertNotNull(list);
-				assertEquals(0, list.size());
-			});
-	}
-
-	protected abstract void validate(final T object, final U id, final StravaResourceState state);
 
 	protected abstract void validate(final T object);
+
+	protected abstract void validate(final T object, final U id, final StravaResourceState state);
 
 	protected void validateList(final List<T> list) {
 		for (final T entry : list) {
@@ -197,7 +208,5 @@ public abstract class PagingListMethodTest<T, U> extends StravaTest {
 			validate(entry);
 		}
 	}
-
-	protected abstract ListCallback<T> callback();
 
 }
