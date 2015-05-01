5174bf5cf7847b020e4881827d299ac219dd089c
Dan Shannon <dan.shannon@marlo.com.au>
2015-05-01 16:33:24 +1000

    Merge fail

--- src/main/java/test/api/rest/activity/ListFriendsActivitiesTest.java
+++ src/main/java/test/api/rest/activity/ListFriendsActivitiesTest.java
@@ -5,14 +5,6 @@ import static org.junit.Assert.fail;
 
 import java.time.ZonedDateTime;
 import java.util.Arrays;
-<<<<<<< HEAD
-=======
-
-import javastrava.api.v3.model.StravaActivity;
-import javastrava.api.v3.model.StravaAthlete;
-import javastrava.api.v3.service.exception.UnauthorizedException;
-import javastrava.config.StravaConfig;
->>>>>>> branch 'master' of https://github.com/danshannon/javastrava-test.git
 
 import org.junit.Test;
 
@@ -29,7 +21,6 @@ import test.utils.TestUtils;
 
 public class ListFriendsActivitiesTest extends APIListTest<StravaActivity, Integer> {
 	/**
-<<<<<<< HEAD
 	 *
 	 */
 	public ListFriendsActivitiesTest() {
@@ -88,13 +79,6 @@ public class ListFriendsActivitiesTest extends APIListTest<StravaActivity, Integ
 				}
 			}
 		} );
-=======
-	 * No-arguments constructor provides the required callbacks
-	 */
-	public ListFriendsActivitiesTest() {
-		this.listCallback = (api, id) -> api.listFriendsActivities(null, null);
-		this.pagingCallback = paging -> api().listFriendsActivities(paging.getPage(), paging.getPageSize());
->>>>>>> branch 'master' of https://github.com/danshannon/javastrava-test.git
 	}
 
 	/**
@@ -119,12 +103,8 @@ public class ListFriendsActivitiesTest extends APIListTest<StravaActivity, Integ
 
 			assertNotNull("Returned null array for latest friends' activities", activities);
 
-<<<<<<< HEAD
 			// Check that the activities are returned in descending order of
 			// start date
-=======
-			// Check that the activities are returned in descending order of start date
->>>>>>> branch 'master' of https://github.com/danshannon/javastrava-test.git
 			ZonedDateTime lastStartDate = null;
 			for (final StravaActivity activity : activities) {
 				if (lastStartDate == null) {
@@ -135,48 +115,8 @@ public class ListFriendsActivitiesTest extends APIListTest<StravaActivity, Integ
 					}
 				}
 				StravaActivityTest.validateActivity(activity);
-<<<<<<< HEAD
-=======
 			}
-		});
-	}
-
-	@Test
-	public void testListFriendsActivities_checkPrivateFlagAuthenticatedUser() throws Exception {
-		RateLimitedTestRunner.run(() -> {
-			if (new Issue96().isIssue()) {
-				return;
->>>>>>> branch 'master' of https://github.com/danshannon/javastrava-test.git
-			}
-<<<<<<< HEAD
 		} );
-=======
-			final StravaActivity[] activities = api().listFriendsActivities(1, StravaConfig.MAX_PAGE_SIZE);
-			for (final StravaActivity activity : activities) {
-				if (activity.getAthlete().getId().equals(TestUtils.ATHLETE_AUTHENTICATED_ID) && activity.getPrivateActivity()) {
-					fail("Returned private activities belonging to the authenticated user");
-				}
-			}
-		});
-	}
-
-	@Test
-	public void testListFriendsActivities_checkPrivateFlagOtherUsers() throws Exception {
-		RateLimitedTestRunner.run(() -> {
-			final StravaActivity[] activities = api().listFriendsActivities(1, StravaConfig.MAX_PAGE_SIZE);
-			for (final StravaActivity activity : activities) {
-				if (!(activity.getAthlete().getId().equals(TestUtils.ATHLETE_AUTHENTICATED_ID)) && activity.getPrivateActivity()) {
-					fail("Returned private activities belonging to other users!");
-				}
-			}
-		});
-	}
-
-	@Override
-	protected void validate(final StravaActivity activity) {
-		StravaActivityTest.validateActivity(activity);
-
->>>>>>> branch 'master' of https://github.com/danshannon/javastrava-test.git
 	}
 
 	@Override
@@ -190,7 +130,6 @@ public class ListFriendsActivitiesTest extends APIListTest<StravaActivity, Integ
 		super.testPageNumberAndSize();
 	}
 
-<<<<<<< HEAD
 	@Override
 	protected void validate(final StravaActivity activity) {
 		StravaActivityTest.validateActivity(activity);
@@ -227,62 +166,6 @@ public class ListFriendsActivitiesTest extends APIListTest<StravaActivity, Integ
 	@Override
 	protected Integer validIdNoChildren() {
 		return TestUtils.ATHLETE_WITHOUT_FRIENDS;
-=======
-	/**
-	 * @see test.api.rest.APIListTest#invalidId()
-	 */
-	@Override
-	protected Integer invalidId() {
-		return null;
-	}
-
-	/**
-	 * @see test.api.rest.APIListTest#privateId()
-	 */
-	@Override
-	protected Integer privateId() {
-		return null;
-	}
-
-	/**
-	 * @see test.api.rest.APIListTest#privateIdBelongsToOtherUser()
-	 */
-	@Override
-	protected Integer privateIdBelongsToOtherUser() {
-		return null;
-	}
-
-	/**
-	 * @see test.api.rest.APIListTest#validateArray(java.lang.Object[])
-	 */
-	@Override
-	protected void validateArray(final StravaActivity[] list) {
-		StravaActivityTest.validateList(Arrays.asList(list));
-	}
-
-	/**
-	 * @see test.api.rest.APIListTest#validId()
-	 */
-	@Override
-	protected Integer validId() {
-		return TestUtils.ACTIVITY_FOR_AUTHENTICATED_USER;
-	}
-
-	/**
-	 * @see test.api.rest.APIListTest#validIdBelongsToOtherUser()
-	 */
-	@Override
-	protected Integer validIdBelongsToOtherUser() {
-		return null;
-	}
-
-	/**
-	 * @see test.api.rest.APIListTest#validIdNoChildren()
-	 */
-	@Override
-	protected Integer validIdNoChildren() {
-		return null;
->>>>>>> branch 'master' of https://github.com/danshannon/javastrava-test.git
 	}
 
 }
